fdb58544317907b52be9b9f01e6f48aab9ef7a7e
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-01-25 18:50:37 +0100

    JsonObject#addAll

--- commons-gson/src/main/kotlin/xyz/xenondevs/commons/gson/JsonObjects.kt
+++ commons-gson/src/main/kotlin/xyz/xenondevs/commons/gson/JsonObjects.kt
@@ -27,17 +27,17 @@ inline fun JsonObject.getDoubleOrNull(key: String): Double? = if (hasNumber(key)
 inline fun JsonObject.getObjectOrNull(key: String): JsonObject? = if (hasObject(key)) get(key).asJsonObject else null
 inline fun JsonObject.getArrayOrNull(key: String): JsonArray? = if (hasArray(key)) get(key).asJsonArray else null
 
-inline fun JsonObject.getString(key: String): String = if (hasString(key)) get(key).asString else throw NoSuchElementException("No String with key $key found!")
-inline fun JsonObject.getBoolean(key: String): Boolean = if (hasBoolean(key)) get(key).asBoolean else throw NoSuchElementException("No Boolean with key $key found!")
-inline fun JsonObject.getNumber(key: String): Number = if (hasNumber(key)) get(key).asNumber else throw NoSuchElementException("No Number with key $key found!")
-inline fun JsonObject.getByte(key: String): Byte = if (hasNumber(key)) get(key).asByte else throw NoSuchElementException("No Number with key $key found!")
-inline fun JsonObject.getShort(key: String): Short = if (hasNumber(key)) get(key).asShort else throw NoSuchElementException("No Number with key $key found!")
-inline fun JsonObject.getInt(key: String): Int = if (hasNumber(key)) get(key).asInt else throw NoSuchElementException("No Number with key $key found!")
-inline fun JsonObject.getLong(key: String): Long = if (hasNumber(key)) get(key).asLong else throw NoSuchElementException("No Number with key $key found!")
-inline fun JsonObject.getFloat(key: String): Float = if (hasNumber(key)) get(key).asFloat else throw NoSuchElementException("No Number with key $key found!")
-inline fun JsonObject.getDouble(key: String): Double = if (hasNumber(key)) get(key).asDouble else throw NoSuchElementException("No Number with key $key found!")
-inline fun JsonObject.getObject(key: String): JsonObject = if (hasObject(key)) get(key).asJsonObject else throw NoSuchElementException("No JsonObject with key $key found!")
-inline fun JsonObject.getArray(key: String): JsonArray = if (hasArray(key)) get(key).asJsonArray else throw NoSuchElementException("No JsonArray with key $key found!")
+inline fun JsonObject.getString(key: String): String = if (hasString(key)) get(key).asString else throw NoSuchElementException("No String with key $key found.")
+inline fun JsonObject.getBoolean(key: String): Boolean = if (hasBoolean(key)) get(key).asBoolean else throw NoSuchElementException("No Boolean with key $key found.")
+inline fun JsonObject.getNumber(key: String): Number = if (hasNumber(key)) get(key).asNumber else throw NoSuchElementException("No Number with key $key found.")
+inline fun JsonObject.getByte(key: String): Byte = if (hasNumber(key)) get(key).asByte else throw NoSuchElementException("No Number with key $key found.")
+inline fun JsonObject.getShort(key: String): Short = if (hasNumber(key)) get(key).asShort else throw NoSuchElementException("No Number with key $key found.")
+inline fun JsonObject.getInt(key: String): Int = if (hasNumber(key)) get(key).asInt else throw NoSuchElementException("No Number with key $key found.")
+inline fun JsonObject.getLong(key: String): Long = if (hasNumber(key)) get(key).asLong else throw NoSuchElementException("No Number with key $key found.")
+inline fun JsonObject.getFloat(key: String): Float = if (hasNumber(key)) get(key).asFloat else throw NoSuchElementException("No Number with key $key found.")
+inline fun JsonObject.getDouble(key: String): Double = if (hasNumber(key)) get(key).asDouble else throw NoSuchElementException("No Number with key $key found.")
+inline fun JsonObject.getObject(key: String): JsonObject = if (hasObject(key)) get(key).asJsonObject else throw NoSuchElementException("No JsonObject with key $key found.")
+inline fun JsonObject.getArray(key: String): JsonArray = if (hasArray(key)) get(key).asJsonArray else throw NoSuchElementException("No JsonArray with key $key found.")
 
 inline fun <reified T : JsonElement> JsonObject.getOrPut(key: String, defaultValue: () -> T): T {
     var value = getOrNull(key)
@@ -47,4 +47,8 @@ inline fun <reified T : JsonElement> JsonObject.getOrPut(key: String, defaultVal
     }
     
     return value
+}
+
+fun JsonObject.addAll(other: JsonObject) {
+    other.entrySet().forEach { (property, value) -> add(property, value) }
 }
