0d4ab6a1ef7a80e1f2fac6c8e370e7cd405394b4
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-04-29 18:31:17 +0200

    MethodHandle utility functions

--- commons-reflection/src/main/kotlin/xyz/xenondevs/commons/reflection/Functions.kt
+++ commons-reflection/src/main/kotlin/xyz/xenondevs/commons/reflection/Functions.kt
@@ -1,7 +1,11 @@
 package xyz.xenondevs.commons.reflection
 
+import java.lang.invoke.MethodHandle
+import java.lang.invoke.MethodHandles
+import java.lang.reflect.Method
 import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
+import kotlin.reflect.jvm.javaMethod
 
 fun <T> KFunction<T>.hasEmptyArguments(): Boolean {
     val params = parameters
@@ -9,3 +13,15 @@ fun <T> KFunction<T>.hasEmptyArguments(): Boolean {
     if (params.isEmpty()) return true
     return params.size == 1 && params[0].kind == KParameter.Kind.INSTANCE
 }
+
+fun Method.toMethodHandle(): MethodHandle =
+    MethodHandles.lookup().unreflect(this)
+
+fun Method.toMethodHandle(instance: Any): MethodHandle =
+    MethodHandles.lookup().unreflect(this).bindTo(instance)
+
+fun KFunction<*>.toMethodHandle(): MethodHandle =
+    MethodHandles.lookup().unreflect(javaMethod!!)
+
+fun KFunction<*>.toMethodHandle(instance: Any): MethodHandle =
+    MethodHandles.lookup().unreflect(javaMethod!!).bindTo(instance)
