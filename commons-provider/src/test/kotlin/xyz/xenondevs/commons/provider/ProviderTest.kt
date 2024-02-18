c4764f1f37dfcf742b1dd97580da3ef71913dd88
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-02-18 21:57:16 +0100

    Add unit tests for provider value propagation

---
+++ commons-provider/src/test/kotlin/xyz/xenondevs/commons/provider/ProviderTest.kt
@@ -0,0 +1,63 @@
+package xyz.xenondevs.commons.provider
+
+import org.junit.jupiter.api.Test
+import xyz.xenondevs.commons.provider.immutable.map
+import kotlin.test.assertEquals
+
+class ProviderTest {
+    
+    @Test
+    fun testProviderPropagate() {
+        var value = 1
+        
+        val top = object : Provider<Int>() {
+            override fun loadValue(): Int = value
+        }
+        
+        val branchA = top.map { it * 10 }
+        val branchB = top.map { it * 100 }
+        
+        val leafA = branchA.map { it + 1 }
+        val leafB = branchB.map { it + 1 }
+        
+        assertEquals(top.get(), 1)
+        assertEquals(branchA.get(), 10)
+        assertEquals(branchB.get(), 100)
+        assertEquals(leafA.get(), 11)
+        assertEquals(leafB.get(), 101)
+        
+        value = 2
+        top.update()
+        
+        assertEquals(top.get(), 2)
+        assertEquals(branchA.get(), 20)
+        assertEquals(branchB.get(), 200)
+        assertEquals(leafA.get(), 21)
+        assertEquals(leafB.get(), 201)
+    }
+    
+    @Test
+    fun testProviderUpdateHandler() {
+        var value = 0
+        var invoked = false
+        
+        val provider = object : Provider<Int>() {
+            override fun loadValue(): Int = value
+        }
+        provider.addUpdateHandler { invoked = true }
+        
+        // initializing should not call update handler
+        provider.get()
+        assert(!invoked)
+        
+        // updating without changes to value should not call update handler
+        provider.update()
+        assert(!invoked)
+        
+        // updating with change to value should call update handler
+        value = 1
+        provider.update()
+        assert(invoked)
+    }
+    
+}
---
+++ commons-provider/src/test/kotlin/xyz/xenondevs/commons/provider/mutable/MutableProviderTest.kt
@@ -0,0 +1,67 @@
+package xyz.xenondevs.commons.provider.mutable
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MutableProviderTest {
+    
+    @Test
+    fun testMutableProviderPropagateUp() {
+        val top = mutableProvider(1)
+        val middle = top.map({ it * 10 }, { it / 10 })
+        val bottom = middle.map({ it * 10 }, { it / 10 })
+        
+        assertEquals(1, top.get())
+        assertEquals(10, middle.get())
+        assertEquals(100, bottom.get())
+        
+        bottom.set(200)
+        
+        assertEquals(2, top.get())
+        assertEquals(20, middle.get())
+        assertEquals(200, bottom.get())
+    }
+    
+    @Test
+    fun testMutableProviderPropagateDown() {
+        val top = mutableProvider(1)
+        val middle = top.map({ it * 10 }, { it / 10 })
+        val bottom = middle.map({ it * 10 }, { it / 10 })
+        
+        assertEquals(1, top.get())
+        assertEquals(10, middle.get())
+        assertEquals(100, bottom.get())
+        
+        top.set(2)
+        
+        assertEquals(2, top.get())
+        assertEquals(20, middle.get())
+        assertEquals(200, bottom.get())
+    }
+    
+    @Test
+    fun testPropagateUpDown() {
+        val top = mutableProvider(1)
+        
+        val branchAMiddle = top.map({ it * 10 }, { it / 10 })
+        val branchABottom = branchAMiddle.map({ it * 10 }, { it / 10 })
+        
+        val branchBMiddle = top.map({ it + 1 }, { it - 1 })
+        val branchBBottom = branchBMiddle.map({ it + 10 }, { it - 10 })
+        
+        assertEquals(1, top.get())
+        assertEquals(10, branchAMiddle.get())
+        assertEquals(100, branchABottom.get())
+        assertEquals(2, branchBMiddle.get())
+        assertEquals(12, branchBBottom.get())
+        
+        branchABottom.set(200)
+        
+        assertEquals(200, branchABottom.get())
+        assertEquals(20, branchAMiddle.get())
+        assertEquals(2, top.get())
+        assertEquals(3, branchBMiddle.get())
+        assertEquals(13, branchBBottom.get())
+    }
+    
+}
