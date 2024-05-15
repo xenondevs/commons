675b0999c214e6170d2749204c42477a35d6c3e6
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-05-15 16:20:26 +0200

    MutableProvider#defaultsTo

---
+++ commons-provider/src/main/kotlin/xyz/xenondevs/commons/provider/mutable/DefaultingProviders.kt
@@ -0,0 +1,76 @@
+package xyz.xenondevs.commons.provider.mutable
+
+import xyz.xenondevs.commons.provider.Provider
+import xyz.xenondevs.commons.provider.immutable.provider
+
+/**
+ * Creates a new [MutableProvider] that defaults to [value] if the value of [this][MutableProvider] is null.
+ * The default value is propagated upwards when the value of the returned provider is loaded.
+ */
+fun <T : Any> MutableProvider<T?>.defaultsTo(value: T): MutableProvider<T> {
+    val provider = MutableDefaultValueProvider(this, value)
+    addChild(provider)
+    return provider
+}
+
+/**
+ * Creates a new [MutableProvider] that defaults to the value obtained through [provider] if the value of [this][MutableProvider] is null.
+ * The default value is propagated upwards when the value of the returned provider is loaded.
+ * Once the value has been propagated upwards, changes to the value of [provider] will be ignored.
+ */
+fun <T : Any> MutableProvider<T?>.defaultsTo(provider: Provider<T>): MutableProvider<T> {
+    val result = MutableDefaultProviderProvider(this, provider)
+    addChild(result)
+    return result
+}
+
+/**
+ * Creates a new [MutableProvider] that defaults to the value obtained through the [lazyValue] lambda if the value of [this][MutableProvider] is null.
+ * The default value is propagated upwards when the value of the returned provider is loaded.
+ */
+fun <T : Any> MutableProvider<T?>.defaultsToLazily(lazyValue: () -> T): MutableProvider<T> {
+    // naming this function orElse would lead to a resolution ambiguity with defaultsTo(value: T)
+    
+    return defaultsTo(provider(lazyValue))
+}
+
+private abstract class MutableDefaultingProvider<T : Any>(
+    protected val provider: MutableProvider<T?>,
+) : MutableProvider<T>() {
+    
+    override fun loadValue(): T {
+        var value = provider.get()
+        if (value == null)
+            value = applyDefaultValue()
+        
+        return value
+    }
+    
+    protected abstract fun applyDefaultValue(): T
+    
+}
+
+private class MutableDefaultValueProvider<T : Any>(
+    provider: MutableProvider<T?>,
+    private val defaultValue: T
+) : MutableDefaultingProvider<T>(provider) {
+    
+    override fun applyDefaultValue(): T {
+        provider.set(defaultValue, setOf(this))
+        return defaultValue
+    }
+    
+}
+
+private class MutableDefaultProviderProvider<T : Any>(
+    provider: MutableProvider<T?>,
+    private val defaultProvider: Provider<T>
+) : MutableDefaultingProvider<T>(provider) {
+    
+    override fun applyDefaultValue(): T {
+        val defaultValue = defaultProvider.get()
+        provider.set(defaultValue, setOf(this))
+        return defaultValue
+    }
+    
+}
